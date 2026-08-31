39240f915c26825df877a3a5a2004f88f03b88c5
KiraboM <kirabomusoke46@gmail.com>
2026-08-31 16:38:13 +0100

    Set up schemas in schemas/job.py

--- models/job.py
+++ models/job.py
@@ -14,4 +14,4 @@ class StoryJob(Base):
     story_id = Column(Integer, nullable=True)
     error = Column(String, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
-    completed_at = Column(DateTime(timezone=True), server_default=func.now() nullable=True)
+    completed_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
